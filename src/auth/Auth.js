3fd4046e58945091968a12c4ac7f646efe881ecb
Dylan Morozowski <dymacski@gmail.com>
2020-03-17 20:18:26 -0400

    added functionality to pass token after login/signup

--- src/auth/Auth.js
+++ src/auth/Auth.js
@@ -1,110 +1,133 @@
-/*import React from 'react';
-import{Container, Row, Col} from 'reactstrap';
-import Login from './Login';
-import Signup from './Signup';
+import React, { useState } from "react";
+import "./Auth.css";
+import APIURL from "../helpers/environment";
 
+const Auth = props => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [login, setLogin] = useState(false);
 
-const Auth = (props) => {
-    return(
-        <Container className="auth-container">
-            <Row>
-                <Col md="6">
-                    Signup
-                </Col>
-                <Col md="6" className="login-col">
-                    Login
-                </Col>
-            </Row>
-        </Container>
-    
+  const data2 = {
+    email: email,
+    password: password
+  };
 
-)
-}
+  const title = () => {
+    return login ? "Login" : "Signup";
+  };
+  const loginToggle = event => {
+    event.preventDefault();
+    setLogin(!login);
+    setEmail("");
+    setPassword("");
+    //setFirstName('');
+    setName("");
+  };
 
-export default Auth;*/
+  const displaySignupFields = () => {
+    return !login ? (
+      <div>
+        <label htmlFor="name">Name</label>
+        <br />
 
-//import{Container, Row, Col, Button} from 'reactstrap';
-import React, { useState }from 'react';
-import './Auth.css';
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+      </div>
+    ) : null;
+  };
+  const handleSignup = e => {
+    e.preventDefault();
+    console.log("Signup");
+    fetch(`${APIURL}/signin/signup`, {
+      method: "POST",
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        password: password
+      }),
+      headers: new Headers({
+        "Content-Type": "application/json"
+      })
+    })
+      .then(res => res.json())
+      .then(json => props.updateToken(json.sessionToken))
+      .catch(err => console.log(err));
+  };
 
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log("Signin");
+    fetch(`${APIURL}/signin/login`, {
+      method: "POST",
+      headers: new Headers({
+        "Content-Type": "application/json"
+      }),
+      body: JSON.stringify(data2)
+    })
+      .then(res => res.json())
+      .then(res => console.log(res))
+      .then(json => props.updateToken(json.sessionToken))
+      .catch(err => console.log(err));
+  };
 
-//const Auth = (props) => {
+  const whichSubmit = e => {
+    e.preventDefault();
+    return;
+  };
 
-    const Auth = (props) => {
-       // const [loginUp, setLoginUp] = useState(false);
-       // const [signupUp, setSignupUp] = useState(false);
-      
+  return (
+    <div>
+      <div className="Form">
+        <div className="Container">
+          <div className="Col">
+            <div className="Row">
+              <form
+                onSubmit={e => {
+                  login ? handleSubmit(e) : handleSignup(e);
+                }}
+              >
+                {/* ToDo: change the title based on what form it is Login/Signup*/}
+                <h1>{title()}</h1>
+                {displaySignupFields()}
+                <label htmlFor="email">Email</label>
 
-    //need first, last name, email, password, login(boolean)
-   // const [firstName, setFirstName] = useState('');
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [login, setLogin] = useState(false);
-
-    const title = () => {
-        return login ? 'Login' : 'Signup';
-
-    }
-    const loginToggle = (event) => {
-        event.preventDefault();
-        setLogin(!login);
-        setEmail('');
-        setPassword('');
-        //setFirstName('');
-        setName('');
-    }
-
-    const displaySignupFields = ()=> {
-        return !login ? (
-            <div>
-                
-        
-                <label htmlFor="name">Name</label>
                 <br />
 
-                <input type="text" id= "name" value={name} onChange={(e) => setName(e.target.value)}/>
+                <input
+                  type="text"
+                  id="email"
+                  value={email}
+                  onChange={event => setEmail(event.target.value)}
+                />
+                <br />
 
+                <label htmlFor="password">Password:</label>
+                <br />
+                <input
+                  type="password"
+                  id="password"
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                />
+                <br />
+
+                <button onClick={loginToggle}>Login/Signup</button>
+
+                <br />
+
+                <button type="submit">Submit User Data</button>
+              </form>
             </div>
-        ): null;
-    }
-
-    return (
-        <div>
-            <div className = "Form">
-                <div className ="Container">
-                <div className = "Col">
-                    <div className = "Row">
-        <form action="">
-        {/* ToDo: change the title based on what form it is Login/Signup*/}
-            <h1>{ title()}</h1>
-            {displaySignupFields() }
-            <label htmlFor='email'>Email</label>
-
-            <br />
-
-            <input type='text' id='email' value={email} onChange={(event)=>setEmail(event.target.value)}/>
-            <br />
-
-            <label htmlFor='password'>Password:</label>
-            <br />
-            <input type='password' id='password' value={password}onChange={(e)=>setPassword(e.target.value)}/>
-            <br />
-
-            <button onClick={loginToggle}>Login/Signup</button>
-
-            <br />
-
-            <button type="submit">Submit User Data</button>
-
-            </form>   
-            </div>
-            </div> 
+          </div>
         </div>
-        </div>
-        </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Auth;
-
--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -1,11 +1,11 @@
-let APIURL ='';
+let APIURL = "";
 
 switch (window.location.hostname) {
-    case 'localhost' || '127.0.0.1':
-        APIURL = 'http://localhost: 3002';
-        break;
-        case 'amb-rescueapp.herokuapp.com':
-        APIURL = "https://amb-rescueapp.herokuapp.com"
+  case "localhost" || "127.0.0.1":
+    APIURL = "http://localhost:3002";
+    break;
+  case "amb-rescueapp.herokuapp.com":
+    APIURL = "https://amb-rescueapp.herokuapp.com";
 }
 
 export default APIURL;
